e9b3f6b5cd4dda15e6f082e5363f81dd0cb894d7
vovan-u-s <oversoul6661@gmail.com>
2025-09-19 18:59:42 -0500

    vova

--- page/homePage.ts
+++ page/homePage.ts
@@ -4,9 +4,9 @@ export class Homepage {
   title: Locator;
 
   constructor(page: Page) {
-    this.title = page.locator('div[class="slide-media"]  h1[class="hero-title "]')
+    this.title = page.locator('h1[class="hero-title "]')
   }
   async ourMessage(expectedTitle: string): Promise<void> {
-    expect(this.title).toBe(expectedTitle)
+    expect(this.title).toHaveText(expectedTitle)
   }
 }
