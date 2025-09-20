12308fea0cf5532a694adaf457de5ea2eb63c780
vovan-u-s <oversoul6661@gmail.com>
2025-09-19 19:24:10 -0500

    new one

--- page/homePage.ts
+++ page/homePage.ts
@@ -2,11 +2,16 @@ import { expect, Locator, Page } from '@playwright/test'
 export class Homepage {
 
   title: Locator;
+  message:Locator;
 
   constructor(page: Page) {
     this.title = page.locator('h1[class="hero-title "]')
+    this.message=page.locator('spark-stack[class="search-bar-horizontal-or"]')
   }
   async ourMessage(expectedTitle: string): Promise<void> {
-    expect(this.title).toHaveText(expectedTitle)
+    await expect(this.title).toHaveText(expectedTitle)
+  }
+  async myMessage(expectedMessage:string):Promise<void>{
+    await expect(this.message).toHaveText(expectedMessage)
   }
 }
